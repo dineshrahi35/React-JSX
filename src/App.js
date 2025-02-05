b86c6f57d9416db7c7926d9d82e0f110fbdce0d3
DineshRahi <dineshrahi35@gmail.com>
2025-02-05 21:57:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Sidebar1 from './components/Sidebar1';
-import Main from './components/Main';
-import Footer from './components/Footer';
+// import React from 'react';
+// import Navbar from './components/Navbar';
+// import Sidebar from './components/Sidebar';
+// import Sidebar1 from './components/Sidebar1';
+// import Main from './components/Main';
+// import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <div className='app'>
-      <Navbar />
-      <Main />
-      <div className = 'sidebar'>
-      <Sidebar />
-      <Sidebar1 />
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <Navbar />
+//       <Main />
+//       <div className = 'sidebar'>
+//       <Sidebar />
+//       <Sidebar1 />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { Component } from 'react';
+import CBCPropEx from './propsex/CBCPropEx';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <CBCPropEx
+          username="Janardhan"
+          age={20}
+          hobbies={["Playing", "Reading books", "Watching YouTube"]}
+          address={{ city: "Jangoan", area: "RamaChandraPuram" }}
+          sendFun={() => alert("Hi, I am Janardhan")}
+        />
       </div>
-      <Footer />
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default App;
+
+//       import React, { Component } from 'react';
+//       import CBCPropEx from './propsex/CBCPropEx'; // Assuming CBCPropEx is correctly defined in the specified path
+      
+//       export default class App extends Component {
+//         render() {
+//           return (
+//             <div>
+//               <CBCPropEx
+//                 username="Janardhan"
+//                 isMarried={true}
+//                 hobbies={["Playing", "Reading books", "Watching YouTube"]}
+//               />
+//             </div>
+//           );
+//         }
+//       }
+      
+
+
+
+
+   
+
+
